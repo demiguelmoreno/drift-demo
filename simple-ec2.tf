90dca9930b7607666c10160d9eaa8308c947cf23
Manuel De Miguel Moreno <mdemiguelmor@paloaltonetworks.com>
2022-06-16 21:59:43 +0200

    Update simple-ec2.tf

--- simple-ec2.tf
+++ simple-ec2.tf
@@ -14,13 +14,6 @@ resource "aws_security_group" "ssh_traffic" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_instance" "web_server_instance" {
