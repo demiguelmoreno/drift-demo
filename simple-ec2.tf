eaa11f0ca600df0a3629cd3a0b0090f111039e42
Manuel De Miguel Moreno <mdemiguelmor@paloaltonetworks.com>
2022-06-16 22:08:03 +0200

    Update simple-ec2.tf

--- simple-ec2.tf
+++ simple-ec2.tf
@@ -4,14 +4,20 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_security_group" "ssh_traffic" {
+resource "aws_security_group" "terraform-security-group" {
   name = "mdemiguelmor-ssh_traffic"
-  description = "Allow SSH inbound traffic"
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
 
